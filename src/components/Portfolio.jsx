b3694e8d3461d54e72c7a1dc5c74c31c45599b12
naseem <naseem.bcs18@iba-suk.edu.pk>
2024-06-05 06:33:01 -0700

    Add changes to new Project iShopping add to portfolio

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -2,22 +2,23 @@ import React from 'react'
 import reactjs from '../../public/pojrcts/todo.png'
 import iShopping from '../../public/pojrcts/iShopping.png'
 function Portfolio() {
+
+  
   const cardItems = [
     
     {
       id:1,
       logo:reactjs,
       name:"Todo List",
-      sourCode:['https://github.com/naseem51214/iList'],
-      demo:['https://i-list.vercel.app/']
+      sourCode:'https://github.com/naseem51214/iList',
+      demo:'https://i-list.vercel.app/'
     },
     {
       id:2,
       logo:iShopping,
       name:"iShopping",
-      
-      sourCode:['https://github.com/naseem51214/iShopping'],
-      demo:['https://i-shopping.vercel.app/']
+      sourCode:'https://github.com/naseem51214/iShopping',
+      demo:'https://i-shopping.vercel.app/'
     }
     // {
     //   id:1,
@@ -57,7 +58,7 @@ function Portfolio() {
       {/* <span className='underline font-semibold'>Featured projects</span> */}
       <div className='grid grid-cols-1 md:grid-cols-3 gap-5 p-3 my-5 h-auto'>
         {
-          cardItems.map(({id,logo,name})=>(
+          cardItems.map(({id,logo,name,sourCode,demo})=>(
             <div className='md:w-[300px] md:h-[400px] border-[2px] rounded-lg p-1 mt-5 shadow-lg cursor-pointer hover:scale-110 duration-300 flex flex-col justify-center'  key={id}>
             <div className='flex justify-center'>
 
@@ -68,10 +69,11 @@ function Portfolio() {
               <p className='px-2 text-gray-700'>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
             </div>
             <div className='px-6 py-4 space-x-3 justify-around'>
-              <button className='w-20 h-10 bg-blue-500 hover:bg-blue-700 text-white font-bold px-4 py-2 rounded'><a href={cardItems[0].demo} target='_blank'>Demo</a></button>
-              <button className='w-30 h-10 bg-green-500 hover:bg-green-700 text-white font-bold px-4 py-2 rounded'><a href={cardItems[0].sourCode} target='_blank'>Source Code</a></button>
+              <button className='w-20 h-10 bg-blue-500 hover:bg-blue-700 text-white font-bold px-4 py-2 rounded'><a href={demo} target='_blank'>Demo</a></button>
+              <button className='w-30 h-10 bg-green-500 hover:bg-green-700 text-white font-bold px-4 py-2 rounded'><a href={sourCode} target='_blank'>Source Code</a></button>
             </div>
             </div>
+            
           ))
         }
       </div>
